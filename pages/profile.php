3917e416086350b0c2be21460f2e276b15ce8d1f
shakiel17 <114818927+shakiel17@users.noreply.github.com>
2024-05-19 17:16:59 +0800

    Update profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -22,15 +22,16 @@
                                <input type="text" hidden name="user_id" value="<?=$login_user?>">
                              <div class="form-group">
                                 <label for="password">new password</label>
-                                <input type="password" class="form-control" id="password" name="password" placeholder="New password">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="New password" onchange="CheckPassword(this);">
                               </div>
+                              <span id="passwordremarks"></span>
                               <div class="form-group">
                                 <label for="c_password">Confirm password</label>
                                 <input type="password" class="form-control" id="c_password" name="c_password" placeholder="Confirm password">
                               </div>
                             
                                 <div class="form-group">
-                                  <button type="submit" class="btn btn-primary btn-block mt-4 rounded-0">Update user</button>
+                                  <button type="submit" class="btn btn-primary btn-block mt-4 rounded-0" id="btnprofile">Update user</button>
                                 </div>
                                   </form>
                             <?php 
@@ -48,4 +49,27 @@
           </section>
           <!-- /.content -->
         </div>
-        <!-- /.content-wrapper
+        <!-- /.content-wrapper-->
+
+          <script>
+            function CheckPassword(inputtxt) 
+            { 
+              var strong=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+              var moderate=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/;
+              var weak = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
+              if(inputtxt.value.match(strong)) 
+              { 
+                document.getElementById("passwordremarks").innerHTML = "<font color='green'>Strong Password</font>";
+                document.getElementById("btnprofile").disabled = false;
+              }
+              else if(inputtxt.value.match(moderate))
+              { 
+                document.getElementById("passwordremarks").innerHTML = "<font color='orange'>Moderate Password</font>";
+                document.getElementById("btnprofile").disabled = true;
+              }else{
+                document.getElementById("passwordremarks").innerHTML = "<font color='red'>Poor Password</font>";
+                document.getElementById("btnprofile").disabled = true;
+              }
+            } 
+
+          </script>
